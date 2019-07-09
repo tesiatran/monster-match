c9dc69c87dedbdfb432d822ffc0f57cfdf682db3
Tesia Tran <tesiatran88@gmail.com>
2019-07-09 16:24:34 -0700

    added match two cards feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,79 @@
 $(document).ready(intitializeApp);
 
+var firstCardClicked = null;
+var secondCardClicked = null;
+var firstCardBack = null;
+var secondCardBack = null;
+var matches = null;
+// var maxMatches = 2;
+var firstImage = null;
+var secondImage = null;
+// var attempts = null;
+// var gamesPlayed = null;
+
 function intitializeApp(){
-   $(".cards").on("click", handleCardClick)
+   $(".cards").on("click", handleCardClick);
 }
 
 function handleCardClick(event){
    console.log(event);
-   $(event.currentTarget).find(".cardBack").addClass("hidden");
+
+   if(firstCardClicked === null){
+      firstCardBack = $(event.target).addClass("hidden");
+
+      firstCardClicked = $(event.currentTarget.firstElementChild);
+      firstImage = firstCardClicked.css("background-image");
+
+   } else{
+      secondCardBack = $(event.target).addClass("hidden");
+
+      secondCardClicked = $(event.currentTarget.firstElementChild);
+      secondImage = secondCardClicked.css("background-image");
+
+      if(firstImage === secondImage){
+
+         console.log("cards match");
+         firstCardClicked = null;
+         secondCardClicked = null;
+         firstImage = null;
+         secondImage = null;
+         matches++;
+         // attempts++;
+
+      } else{
+         console.log("flip card back");
+         setTimeout(flipCardBack, 800);
+         // attempts++;
+
+      }
+   }
+
+   // displayStats();
+
+   // if(matches = maxMatches){
+   //    $(".modalContainer").removeClass("hidden");
+   //    gamesPlayed++;
+   // }
 }
 
+function flipCardBack(){
+   firstCardBack.removeClass("hidden");
+   secondCardBack.removeClass("hidden");
+   firstCardBack = null;
+   secondCardBack = null;
+   firstCardClicked = null;
+   secondCardClicked = null;
+   firstImage = null;
+   secondImage = null;
+}
+
+// function calculateAccuracy(){
+//    var accuracyPercentage = (matches / attempts) * 100;
+//    return accuracyPercentage + "%";
+// }
+
+// function displayStats(){
+      // var accuracy = calculateAccuracy();
+      // $(".accuracyPercentage").text(accuracy);
+      // $(".gamesAttempted").text(attempts);
+// }
