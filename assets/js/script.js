24c5f5e5c3ca49e990e359a8d81e650a3817e291
Tesia Tran <tesiatran88@gmail.com>
2019-07-08 17:20:11 -0700

    added ability to flip one card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
+$(document).ready(intitializeApp);
+
+function intitializeApp(){
+   $(".cards").on("click", handleCardClick)
+}
+
+function handleCardClick(event){
+   console.log(event);
+   $(event.currentTarget).find(".cardBack").addClass("hidden");
+}
 
