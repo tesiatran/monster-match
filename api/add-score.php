220ac2f68c19f766997dfa2f62823e58521e0570
Tesia Tran <tesiatran88@gmail.com>
2019-10-19 22:21:53 -0700

    Added body, object, result, name, attempts, and accuracy variables to add-score.php file.

--- api/add-score.php
+++ api/add-score.php
@@ -4,6 +4,14 @@ require_once('functions.php');
 set_exception_handler('error_handler');
 require_once('db_connection.php');
 
+$body = file_get_contents('php://input');
+$object = json_decode($body);
+$result = (array) $object;
+
+$name = $result['name'];
+$attempts = $result['attempts'];
+$accuracy = $result['accuracy'];
+
 
 
 ?>
