d871c5fc3b72ec0169f7ece11f099d405561808d
Tesia Tran <tesiatran88@gmail.com>
2019-10-26 23:55:34 -0700

    Added query for high scores to high-score.php file.

--- api/high-score.php
+++ api/high-score.php
@@ -5,4 +5,15 @@ set_exception_handler('error_handler');
 startup();
 require_once('db_connection.php');
 
+$scoreQuery = "SELECT id, name, attempts, accuracy
+  FROM `highScore`
+  ORDER BY `attempts`
+  LIMIT 5";
+
+  $scoreResult = mysqli_query($conn, $scoreQuery);
+
+  if(!$scoreResult) {
+    throw new Exception("Connection Failed: " . mysqli_error($conn));
+  }
+
 ?>
