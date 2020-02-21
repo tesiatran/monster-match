eaf47ac8e8830c2a6b12a24421d74d5dfeecded3
Tesia Tran <tesiatran88@gmail.com>
2020-02-21 03:07:30 -0800

    Renamed retrieve scores file. Updating functions.php file.

--- api/functions.php
+++ api/functions.php
@@ -5,6 +5,7 @@ function error_handler($error) {
     "success" => false,
     "error" => $error->getMessage()
   ];
+  
   http_response_code(500);
   $json_output = json_encode($output);
   print($json_output);
--- api/submit-score.php
+++ api/submit-score.php
@@ -1,16 +1,16 @@
 <?php
 
-require_once('functions.php');
-set_exception_handler('error_handler');
-require_once('db_connection.php');
+require_once("functions.php");
+set_exception_handler("error_handler");
+require_once("db_connection.php");
 
-$body = file_get_contents('php://input');
+$body = file_get_contents("php://input");
 $object = json_decode($body);
 $result = (array) $object;
 
-$name = $result['name'];
-$attempts = $result['attempts'];
-$accuracy = $result['accuracy'];
+$name = $result["name"];
+$attempts = $result["attempts"];
+$accuracy = $result["accuracy"];
 
 $insertQuery = "INSERT INTO `highscore`
   (`name`, `attempts`, `accuracy`)
