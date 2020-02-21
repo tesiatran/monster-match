7990751c6b8a572e3ffed234123537717dc0b418
Tesia Tran <tesiatran88@gmail.com>
2020-02-21 03:41:33 -0800

    Redoing submit-score file.

--- api/submit-score.php
+++ api/submit-score.php
@@ -3,23 +3,10 @@
 require_once("functions.php");
 set_exception_handler("error_handler");
 require_once("db_connection.php");
+startup();
 
-$body = file_get_contents("php://input");
-$object = json_decode($body);
-$result = (array) $object;
+$bodyData = getBodyData();
 
-$name = $result["name"];
-$attempts = $result["attempts"];
-$accuracy = $result["accuracy"];
 
-$insertQuery = "INSERT INTO `highscore`
-  (`name`, `attempts`, `accuracy`)
-  VALUES (`$name`, `$attempts`, `$accuracy`)";
-
-$insertResult = mysqli_query($conn, $insertQuery);
-
-if(!$insertResult) {
-  throw new Exception("Failed to add data to table " . $insertResult);
-}
 
 ?>
